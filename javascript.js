f6b712587fc4e7a696e3f1b3ef2f8b50853e04ba
SatyamGangani <75199594+SatyamGangani@users.noreply.github.com>
2022-08-30 10:48:48 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -39,6 +39,9 @@ function func(){
     }
      
     if(!(validName.style.display == 'block' || validEmail.style.display == 'block' || validPhone.style.display == 'block' || validMessage.style.display == 'block'  )){
-        window.location.replace('http://127.0.0.1:5500/message.html')
+       a =  window.location.href
+       b = a.replace('#contact','message.html').replace('index.html' , 'message.html').replace('#about' , 'message.html').replace('#','message.html').replace('#products','message.html')
+       window.location.replace(b)
+
     }
 }
